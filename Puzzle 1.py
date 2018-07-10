533d25f03b5d3af874b71a3d040ab0cd66e39743
Mahesh Gaikwad <39650869+concretesteelbond@users.noreply.github.com>
2018-07-10 19:24:58 +0530

    math puzzle one

--- Puzzle 1.py
+++ Puzzle 1.py
@@ -1,6 +1,6 @@
 
 '''If 1 Horse = Rs. 1, 1 Elephant = Rs. 5, 20 Camels = Rs. 1/- then Find 
-the combination of animals so that 100 animals = Rs.100 '''
+the combination of animals so that 100 animals = 100 rs '''
 
 ## Given Data
 horse = 1 #$
