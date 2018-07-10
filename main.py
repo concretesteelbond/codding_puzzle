e0e80ed39ea65fd9a88aaad3bf0d73bbbb1d30b2
Mahesh Gaikwad <39650869+concretesteelbond@users.noreply.github.com>
2018-07-10 19:17:00 +0530

    Python Code
    spoiler

---
+++ main.py
@@ -0,0 +1,25 @@
+
+
+## Given Data
+horse = 1 #$
+elephant = 5 #$
+camel = 1/20 #$
+## OutPut should be
+total_animal = 100
+total_money = 100
+
+x = list(range(1,97))
+y = list(range(1,97))
+z = list(range(1,97))
+for x1 in x:
+    for y1 in y:
+        for z1 in z:
+            if  (x1*horse + y1*elephant + z1*camel == total_money
+                 and x1+y1+z1 == total_animal):
+                print("Horse = "+str(x1) ,
+                      "Elephant = "+str(y1),
+                      "Camel = " +str(z1))
+                
+                
+        
+                
