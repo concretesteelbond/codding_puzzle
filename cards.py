f8ae12bedb151c7ab35d4748d645d897daa3553c
Mahesh Gaikwad <39650869+concretesteelbond@users.noreply.github.com>
2018-07-11 18:01:51 +0530

    Add files via upload

---
+++ cards.py
@@ -0,0 +1,38 @@
+# Card Puzzle
+num = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"]
+c_type = ["C","D","H","S"]
+all_cards = []
+for t_each in c_type:
+    for n_each in num:
+        all_cards.append((t_each,n_each))
+#print(all_cards)     
+first_card = eval(input())
+second_card = eval(input())
+third_card = eval(input())
+forth_card =  eval(input())
+if all_cards.index(second_card) > all_cards.index(third_card):
+    if all_cards.index(third_card)> all_cards.index(forth_card):
+        ans = 6 #"LMS" 
+    else:
+        if all_cards.index(second_card)> all_cards.index(forth_card):
+            ans = 5 #"LSM"
+        else:
+            ans = 3 #"MSL"      
+else:
+    if all_cards.index(third_card)> all_cards.index(forth_card):
+        if all_cards.index(second_card)> all_cards.index(forth_card):
+            ans = 4 #"MLS"
+        else:
+            ans = 2 #"SLM"
+    else:
+        ans = 1 #"SML"
+print(ans)
+ans1 = ans + (num.index(first_card[1])) + 1
+if ans1 > len(num):
+    ans1 = - (len(num) - ans1)  - 1
+    print(first_card[0], num[ans1])
+else:
+   print(first_card[0], num[ans1-1]) 
+
+
+    
