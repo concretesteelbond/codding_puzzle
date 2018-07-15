2d65e590eb1f8b6089fb17f8b54d8c3f8a856a2f
Mahesh Gaikwad <39650869+concretesteelbond@users.noreply.github.com>
2018-07-15 17:20:28 +0530

    Card puzzle

--- puzzle_3.py
+++ puzzle_3.py
@@ -6,18 +6,22 @@ optimal order; your code must guess the 5th card.'''
 # www.concretesteelbond.com
 
 # Preparing Card Deck
-num = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"]
+import os
+from PIL import Image
+import tkinter as tk
+import time
+
+num = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
 c_type = ["C","D","H","S"]
 all_cards = []
 for t_each in c_type:
     for n_each in num:
-        all_cards.append((t_each,n_each))
-#print(all_cards)   
-####
-first_card = eval(input())
-second_card = eval(input())
-third_card = eval(input())
-forth_card =  eval(input())
+        all_cards.append((t_each+n_each))
+#print(all_cards)     
+first_card = input("First card = ")
+second_card = input("Second card = ")
+third_card = input("Third card = ")
+forth_card = input("Fourth card = ")
 if all_cards.index(second_card) > all_cards.index(third_card):
     if all_cards.index(third_card)> all_cards.index(forth_card):
         ans = 6 #"LMS" 
@@ -34,13 +38,49 @@ else:
             ans = 2 #"SLM"
     else:
         ans = 1 #"SML"
-print(ans)
-ans1 = ans + (num.index(first_card[1])) + 1
+#print(ans)
+#print(ans)
+ans1 = ans + (num.index(first_card[1::])) + 1
 if ans1 > len(num):
     ans1 = - (len(num) - ans1)  - 1
-    print(first_card[0], num[ans1])
+    correct_card = first_card[0]+num[ans1]
 else:
-   print(first_card[0], num[ans1-1]) 
-
-
+    correct_card = first_card[0]+num[ans1-1]
     
+print("The Fifth card is ...... ",correct_card)   
+#Img = Image.open("cards\\" + correct_card + ".png")
+#Img.show()
+
+four_card = [first_card,second_card,
+             third_card,forth_card]
+root = tk.Tk()
+root.geometry("700x800")
+
+image1 = tk.PhotoImage(file = "cards\\" + first_card + ".png")
+image1 = image1.subsample(4,4)
+tk.Label(root,image = image1).place(x = 100,y = 100)
+
+image2 = tk.PhotoImage(file = "cards\\" + second_card + ".png")
+image2 = image2.subsample(4,4)
+tk.Label(root,image = image2).place(x = 130,y = 100)
+
+image3 = tk.PhotoImage(file = "cards\\" + third_card + ".png")
+image3 = image3.subsample(4,4)
+tk.Label(root,image = image3).place(x = 160,y = 100)
+
+image4 = tk.PhotoImage(file = "cards\\" + forth_card + ".png")
+image4 = image4.subsample(4,4)
+tk.Label(root,image = image4).place(x = 190,y = 100)
+
+image = tk.PhotoImage(file = "cards\\" + correct_card + ".png")
+image = image.subsample(3,3)
+label = tk.Label(root,image = image)
+label.place(x = 400,y = 100)
+root.mainloop()
+
+
+
+
+
+
+
